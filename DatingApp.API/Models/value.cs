42fe1be3b709ee2e16b711c35fd121bddd2d8d90
Justin Parker <justin.parker@masterelectronics.com>
2020-05-22 12:32:03 -0400

    Initial Commit

---
+++ DatingApp.API/Models/value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Values class
+/// </summary>
+
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Value()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
+
